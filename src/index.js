c2190116463f5695472f2d8056f88d3ddb6a7e5b
Shubidumdu <skaty4870@naver.com>
2020-11-18 23:32:53 +0900

    Create folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import Phaser from 'phaser';
 
+function preload() {}
+
+function create() {}
+
+function update() {}
+
 const config = {
-  width: 500,
-  height: 500,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   backGround: 0x000000,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
 };
 
 const game = new Phaser.Game(config);
