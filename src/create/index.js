05b858e6d50cfc02da99ced3da24d986ca8016d3
Shubidumdu <skaty4870@naver.com>
2020-11-19 23:13:02 +0900

    Make the jet can be draggable

--- src/create/index.js
+++ src/create/index.js
@@ -1,7 +1,18 @@
 function create() {
   this.add.image(400, 500, 'ship');
   this.add.image(400, 50, 'moon');
-  this.add.image(400, 200, 'jet');
+  const jet = this.add.image(400, 200, 'jet').setInteractive();
+  this.input.setDraggable(jet);
+  jet.on('dragstart', function (pointer) {
+    this.setTint(0xff0000);
+  });
+  jet.on('drag', function (pointer, dragX, dragY) {
+    this.x = dragX;
+    this.y = dragY;
+  });
+  jet.on('dragend', function (pointer) {
+    this.clearTint();
+  });
 }
 
 export default create;
--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const config = {
   scene: {
     preload: preload,
     create: create,
-    // update: update,
   },
 };
 
